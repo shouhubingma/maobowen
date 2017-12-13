4605dee31e378591853c6586f0c432cf53681e80
shouhubingma <1595220557@qq.com>
2017-12-13 19:37:08 +0800

    yaoqingma

--- js/index.js
+++ js/index.js
@@ -36,18 +36,18 @@ music.onclick = function () {
 };
 
 
-仅限手机端显示
- var system = {};
-var p = navigator.platform;
-var u = navigator.userAgent;
-
-system.win = p.indexOf("Win") == 0;
-system.mac = p.indexOf("Mac") == 0;
-system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
-if (system.win || system.mac || system.xll) {//如果是PC转
-    if (u.indexOf('Windows Phone') > -1) {  //win手机端
-
-    }else {
-        window.location.href = "1.html";
-    }
-}
+//仅限手机端显示
+// var system = {};
+//var p = navigator.platform;
+//var u = navigator.userAgent;
+//
+//system.win = p.indexOf("Win") == 0;
+//system.mac = p.indexOf("Mac") == 0;
+//system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+//if (system.win || system.mac || system.xll) {//如果是PC转
+//    if (u.indexOf('Windows Phone') > -1) {  //win手机端
+//
+//    }else {
+//        window.location.href = "1.html";
+//    }
+//}
